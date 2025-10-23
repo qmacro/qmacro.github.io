1c78387c93d2ddc58566cea407d9e554b6ff60db
DJ Adams <qmacro@gmail.com>
2025-10-23 11:05:35 +0000

    add social card info

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Website
 
 New home for website content, based on Eleventy v3 and [the base blog repo](https://github.com/11ty/eleventy-base-blog).
-
-Temporary location (while construction continues): <https://qmacro.org/website/>.
--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,12 @@
 export default {
-	title: "DJ Adams",
-	url: "https://qmacro.org/website/",
-	language: "en",
-	description: "Reserving the right to be wrong.",
-	author: {
-		name: "DJ Adams",
-		email: "qmacro@gmail.com",
-		url: "https://qmacro.org/about/"
-	}
+  title: "DJ Adams",
+  url: "https://qmacro.org",
+  image: "site-image.png",
+  language: "en",
+  description: "Reserving the right to be wrong.",
+  author: {
+    name: "DJ Adams",
+    email: "qmacro@gmail.com",
+    url: "https://qmacro.org/about/"
+  }
 }
